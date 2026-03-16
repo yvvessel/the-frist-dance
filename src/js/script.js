cb6e9c2ec2ba608b88eb86fbc670386861ef0ec5
Thiago Dos Santos Gomes <thiago.dossantosgomes032006@gmail.com>
2026-03-16 20:30:04 +0000

    Ajustando caminho dos arquivos

--- src/js/script.js
+++ src/js/script.js
@@ -15,7 +15,7 @@ function getRankImage(rank){
 
   const name = rank.toLowerCase().replace(/\s+/g, "_") + "_Rank.png"
 
-  return `../../../../ranks/${name}`
+  return `./assets/images/ranks/${name}`
 }
 
 async function loadTeam() {
