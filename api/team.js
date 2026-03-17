5270ff04366b20c4b6016d4dddb9b3e561454b62
Thiago Dos Santos Gomes <thiago.dossantosgomes032006@gmail.com>
2026-03-17 01:55:52 +0000

    Ajuste para o vercel

--- api/team.js
+++ api/team.js
@@ -45,6 +45,7 @@ export default async function handler(req, res) {
         };
       })
     );
+    console.log("API_KEY:", API_KEY);
     console.log(mmrData);
     res.status(200).json(results);
 
